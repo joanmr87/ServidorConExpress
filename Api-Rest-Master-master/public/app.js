2432c2a38a14bcb0d0e59dffa42097db019ad0e6
JoanMRomero <joanmr87@gmail.com>
2021-05-26 08:47:27 -0300

    Agrego

--- Api-Rest-Master-master/public/app.js
+++ Api-Rest-Master-master/public/app.js
@@ -3,13 +3,20 @@ const contentTable = document.getElementById('contentTable');
 //Tomo el template y su contenido (.content)
 const templateRow = document.getElementById('contentRow').content;
 
+
+function addRow(name, age) {
 //clono el template en una nueva variable
 const row = templateRow.cloneNode(true);
 
 //Modifico el valor del nodo de texto por el importado por el usuario
-row.querySelector('.txtName').innerText = 'Hola';
-row.querySelector('.txtAge').innerText = 65;
+row.querySelector('.txtName').innerText = name;
+row.querySelector('.txtAge').innerText = age;
 
 contentTable.appendChild(row);
+}
 
-console.log("probando conexion del app.js")
+addRow('kaka', 45);
+addRow('kakd', 34);
+
+
+
--- Api-Rest-Master-master/src/handlers/users/add.js
+++ Api-Rest-Master-master/src/handlers/users/add.js
@@ -1,22 +1,34 @@
-const database = require('../../database')
+const database = require('../../database');
+const { body, validationResult } = require("express-validator");
+const { check } = require('express-validator');
 
 module.exports = (route) => {
 
-    route.post("/", (req, res) => {
-    const user = {        
-        name: req.body.name,
-        age: req.body.age,
-    };
-   
-    const userName = user.name.toLocaleLowerCase().trim();
+    route.post(
+        "/",
+        body("name").notEmpty().withMessage("Campo obligatorio").isLength({ min:3 }).withMessage("Debe tener al menos 3 caracteres"),
+        body("age").notEmpty().withMessage("Campo obligatorio").isInt().withMessage("debe ser un numero"),        
+        (req, res) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                res.status(400).json({ errors: errors.array() });
+                return;
+            }
 
-    if (database.DB.some((item) => item.name.toLocaleLowerCase().trim() == userName )){
-        res.status(409).json({
-            messege: "'name' debe ser unico"
-        });     
-    } else {
-        database.add(user);       
-        res.json(user);
-    }
+        const user = {        
+            name: req.body.name,
+            age: req.body.age,
+        };
+    
+        const userName = user.name.toLocaleLowerCase().trim();
+
+        if (database.DB.some((item) => item.name.toLocaleLowerCase().trim() == userName )){
+            res.status(409).json({
+                messege: "'name' debe ser unico"
+            });     
+        } else {
+            database.add(user);       
+            res.json(user);
+        }
 });
 }
